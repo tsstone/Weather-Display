651563fe5e4dbe8bf1450f58f173f11fcfa7a852
tsstone <tss.stones@gmail.com>
2020-08-10 19:05:45 -0400

    Johnny five test

--- index.js
+++ index.js
@@ -2,25 +2,48 @@ const fetch = require('node-fetch');
 const raspi = require('raspi');
 const gpio = require('raspi-gpio');
 
-
-const key = "2cf1a5736af3359c73a7f38aea1c9965";
-const zip = "40601"
-
-// var Gpio = require('onoff').Gpio;
-// var LED = new Gpio(4, 'out');
-// LED.writeSync(1);
-
-
-raspi.init(() => {
-    getWeather();
-    const shower = new gpio.DigitalOutput(7);
-    console.log(shower);
-    shower.write(1);
-    // const shower = new gpio.DigitalOutput(11);
-    // shower.write(HIGH);
+const five = require("johnny-five");
+const board = new five.Board({
+    io: new raspi()
 });
 
 
+
+const ON = 1;
+const OFF = 0;
+const key = "2cf1a5736af3359c73a7f38aea1c9965";
+const zip = "40601"
+
+
+/* raspi.init(async () => {
+    const weather = await getWeather();
+    const shower = new gpio.DigitalOutput(7);
+    shower.write(0);
+});
+ */
+
+// Run Board 
+board.on('ready', function() { 
+    // LED Pin variable 
+    const led = new five.Led(7); 
+    led.on(); 
+    this.repl.inject({ 
+            on: () => { 
+                    led.on(); 
+            }, 
+            off: () => { 
+                    led.stop().off(); 
+            }, 
+            strobe: () => { 
+                    led.stop().off(); 
+                    led.strobe(); 
+            }, 
+            blink: () => { 
+                    led.stop().off(); 
+                    led.blink(500); 
+            }, 
+    }); 
+});
 async function getWeather(){
     const weatherAPI = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${key}&units=imperial`);
     const weather = await weatherAPI.json();

60b603b6066b75eb7ab021983140c32cb0027b0e
tsstone <tss.stones@gmail.com>
2020-08-10 17:49:48 -0400

    Testing raspi GPIO

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const fetch = require('node-fetch');
+const raspi = require('raspi');
+const gpio = require('raspi-gpio');
 
 const key = "2cf1a5736af3359c73a7f38aea1c9965";
 const zip = "40601"
+
+
+
+raspi.init(() => {
+    const shower = new gpio.DigitalOutput(7);
+    shower.write(HIGH);
+    // const shower = new gpio.DigitalOutput(11);
+    // shower.write(HIGH);
+});
 
 
 async function getWeather(){

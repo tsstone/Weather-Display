7347796e5418c2d930e2029f184d7b8ea3c3645a
tsstone <tss.stones@gmail.com>
2020-08-10 18:04:26 -0400

    Test LED

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const zip = "40601"
 
 raspi.init(() => {
     const shower = new gpio.DigitalOutput(7);
-    shower.write(HIGH);
+    shower.write(LOW);
     // const shower = new gpio.DigitalOutput(11);
     // shower.write(HIGH);
 });
@@ -19,6 +19,7 @@ async function getWeather(){
     const weatherAPI = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${key}&units=imperial`);
     const weather = await weatherAPI.json();
     console.log(weather);
+    shower.write(HIGH);
 }
 
 getWeather();

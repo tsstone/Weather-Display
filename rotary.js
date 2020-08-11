a3f360676a2f41a04ee80da6d74b4bd68d024396
tsstone <tss.stones@gmail.com>
2020-08-11 15:15:45 -0400

    new rotary file

--- rotary.js
+++ rotary.js
@@ -1,28 +1,95 @@
+var Emitter = require("events").EventEmitter;
 const five = require('johnny-five');
 const Raspi = require('raspi-io').RaspiIO;
-const rotaryEncoder = require('johnny-five-rotary-encoder');
+var emitter = new Emitter();
 
 const board = new five.Board({
     io: new Raspi()
 });
 
-board.on('ready', () => {
-    const upButton = new five.Button("GPIO17");
-    const downButton = new five.Button("GPIO27");
-    const pressButton = new five.Button("GPIO22");
+function Encoder(opts) {
+    Emitter.call(this);
   
-    rotaryEncoder({
-      upButton,
-      downButton,
-      pressButton,
-      onUp: () => {
-        console.log('up');
-      },
-      onDown: () => {
-        console.log('down');
-      },
-      onPress: () => {
-        console.log('press');
-      },
+    var last = 0;
+    var lValue = 0;
+    var value = 0;
+    var rotation = 0;
+  
+    var a = new five.Digital(opts.a);
+    var b = new five.Digital(opts.b);
+  
+    var handler = function() {
+      this.emit("data", this.value);
+  
+      var MSB = a.value;
+      var LSB = b.value;
+      var pos, turn;
+  
+      if (LSB === 1) {
+        pos = MSB === 1 ? 0 : 1;
+      } else {
+        pos = MSB === 0 ? 2 : 3;
+      }
+  
+      turn = pos - last;
+  
+      if (Math.abs(turn) !== 2) {
+        if (turn === -1 || turn === 3) {
+          value++;
+        } else if (turn === 1 || turn === -3) {
+          value--;
+        }
+      }
+  
+      last = pos;
+  
+      if (lValue !== value) {
+        this.emit("change", value);
+      }
+  
+      if (value % 80 === 0 && value / 80 !== rotation) {
+        rotation = value / 80;
+        this.emit("rotation");
+      }
+  
+      lValue = value;
+    }.bind(this);
+  
+    a.on("data", handler);
+    b.on("data", handler);
+  
+    Object.defineProperties(this, {
+      value: {
+        get: function() {
+          return value;
+        }
+      }
+    });
+  }
+  
+  Encoder.prototype = Object.create(Emitter.prototype, {
+    constructor: {
+      value: Encoder
+    }
+  });
+  
+  board.on("ready", function() {
+    var encoder = new Encoder({
+      a: "GPIO17",
+      b: "GPIO27"
+    });
+  
+    var button = new five.Button("GPIO22");
+  
+    button.on("press", function() {
+      console.log("pressed");
+    });
+  
+    encoder.on("change", function() {
+      console.log("Encoder At: %d", this.value);
+    });
+  
+    encoder.on("rotation", function() {
+      console.log("Rotations: %d", Math.abs(this.value / 80));
     });
   });
---
+++ rotaryOLD.js
@@ -0,0 +1,28 @@
+const five = require('johnny-five');
+const Raspi = require('raspi-io').RaspiIO;
+const rotaryEncoder = require('johnny-five-rotary-encoder');
+
+const board = new five.Board({
+    io: new Raspi()
+});
+
+board.on('ready', () => {
+    const upButton = new five.Button("GPIO17");
+    const downButton = new five.Button("GPIO27");
+    const pressButton = new five.Button("GPIO22");
+  
+    rotaryEncoder({
+      upButton,
+      downButton,
+      pressButton,
+      onUp: () => {
+        console.log('up');
+      },
+      onDown: () => {
+        console.log('down');
+      },
+      onPress: () => {
+        console.log('press');
+      },
+    });
+  });

4c170d141d9a42217803e63c564d5e6ff7ed5ac4
tsstone <tss.stones@gmail.com>
2020-08-11 15:25:17 -0400

    testing timeout

--- rotaryController.js
+++ rotaryController.js
@@ -28,7 +28,7 @@ module.exports = function rotaryEncoder({
       if (waveform.length < 2) {
         waveformTimeout = setTimeout(() => {
           waveform = '';
-        }, 8);
+        }, 30);
         return;
       }
   
